932e3dbaf985b5f6ce487ac2456790d6fad61d4c
jeff <jeff@legnd.com>
2017-06-23 02:44:10 -0500

    more

--- earth.gd
+++ earth.gd
@@ -7,10 +7,6 @@ var mass = 5.972e24
 func radius():
 	return get_parent().astro_unit
 
-# Find the Sun's position in space.
-func find_sun():
-	return get_node("../Sun").get_pos()
-
 # Angle of the Earth to the Sun at the dawn of time (deg).
 var angle = 0
 
@@ -18,10 +14,11 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	var sun_location = get_node("../Sun").get_pos()
 	var position = get_pos()
 	angle += -delta * (PI / 180) * 100
 	if (angle > 360):
 		angle -= 360
-	position.x = find_sun().x + (cos(angle) * radius())
-	position.y = find_sun().y + (sin(angle) * radius())
+	position.x = sun_location.x + (cos(angle) * radius())
+	position.y = sun_location.y + (sin(angle) * radius())
 	set_pos(position)
