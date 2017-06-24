8044cbc827ead5f50ca3649b934d8046aacc4a95
jeff <jeff@legnd.com>
2017-06-24 11:56:39 -0500

    draw

---
+++ instrument/line.gd
@@ -0,0 +1,15 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+func _draw():
+	draw_line(Vector2(0, 0), Vector2(600, 300), Color(255, 0, 0), 3.0)
+
+#func _process(delta):
+#	update()
+
+#func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+#	set_process(true)
