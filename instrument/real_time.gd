86721d406855dd1f1f519c6ad175c9b4f9bd0714
jeff <jeff@legnd.com>
2017-06-24 00:10:58 -0500

    not sure

--- instrument/real_time.gd
+++ instrument/real_time.gd
@@ -1,10 +1,11 @@
 extends Label
 
-var accum=0
+var accum = 0
 
 func _ready():
     set_process(true)
 
 func _process(delta):
     accum += delta
+
     set_text("Real Time : " + str("%.1f" % accum) + "s")
