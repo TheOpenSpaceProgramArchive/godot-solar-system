33c38649c91c27fec201caff7e484e2a0e1b198e
jeff <jeff@legnd.com>
2017-06-25 17:49:36 -0500

    cleaning

--- map/map.gd
+++ map/map.gd
@@ -1,13 +1,4 @@
 extends Node
 
-func get_sun_node():
-	return get_node("Space/Sun")
-
-func get_earth_node():
-	return get_node("Space/Earth")
-
-func get_earth_position():
-	return get_node("Space/Earth").get_pos()
-
 func axis_length():
 	return 2 * global.au()
