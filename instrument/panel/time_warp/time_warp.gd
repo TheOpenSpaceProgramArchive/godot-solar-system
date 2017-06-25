a4b7391a7e2b39a8e0d5c4052cefd133d8464677
jeff <jeff@legnd.com>
2017-06-25 14:41:54 -0500

    it works!

--- instrument/panel/time_warp/time_warp.gd
+++ instrument/panel/time_warp/time_warp.gd
@@ -2,7 +2,7 @@
 
 extends Label
 
-const TIME_WARP = 1000
+const TIME_WARP = 3153600
 
 func _ready():
 	set_text("Time Warp : " + str("%s" % TIME_WARP) + "x")
--- space/earth.gd
+++ space/earth.gd
@@ -12,10 +12,31 @@ func radius():
 func find_sun():
 	return get_node("../Sun").get_pos()
 
+func orbital_speed():
+	# Find the mass of the Sun.
+	var sunMass = get_node("../Sun").mass # kg
+
+	# Calculate Earth's orbital velocity.
+	return sqrt((global.G * sunMass) / global.AU) * 1e-3 # km/s
+
+	# Show Earth's orbital velocity to one decimal.
+
+
 # Angle of the Earth to the Sun at the dawn of time (deg).
 var angle = 0
 
 func _ready():
+	# Get the Earth's orbital rotation.
+	var orbital_speed = orbital_speed() * 1000 # m / s
+
+	var radius = global.AU # m
+
+	var time_warp = 1000
+
+	var shit = (time_warp * orbital_speed) / radius
+
+	print(shit)
+
 	set_process(true)
 
 func _process(delta):
@@ -23,12 +44,16 @@ func _process(delta):
 	var sun_location = find_sun()
 	# Find your position.
 	var position = get_pos()
+	# Find out how fast your moving.
+	var orbital_speed = orbital_speed() * 1000
 	# Fine the time warp.
 	var time_warp = get_node("../../../Instrument/Panel/Time Warp").TIME_WARP
 	# Init Angular Speed relative to the Sun.
-	angle += -delta * (PI / 180) * time_warp
-	if (angle > 360):
-		angle -= 360
+	angle += -(delta * orbital_speed * time_warp) / global.AU
+
+
+	# if (angle > 2 * PI):
+	#	angle -= 2 * PI
 
 	position.x = sun_location.x + (cos(angle) * radius())
 	position.y = sun_location.y + (sin(angle) * radius())
