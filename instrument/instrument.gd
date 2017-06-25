ae1ef80a9fe4d3f695ecabe3ded984ef8e4a430e
jeff <jeff@legnd.com>
2017-06-25 01:08:13 -0500

    fixed angle sort of

--- instrument/instrument.gd
+++ instrument/instrument.gd
@@ -6,5 +6,8 @@ func get_sun_node():
 func get_earth_node():
 	return get_node("Space/Earth")
 	
+func get_earth_position():
+	return get_node("Space/Earth").get_pos()
+	
 func axis_length():
 	return 2 * global.au()
