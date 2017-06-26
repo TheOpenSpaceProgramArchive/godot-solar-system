ae0ad585cc893aa87e8ff7b4424c875c8dd871ed
jeff <jeff@legnd.com>
2017-06-26 14:04:48 -0500

    clean earth

--- main/space/earth/earth.gd
+++ main/space/earth/earth.gd
@@ -1,36 +1,37 @@
 extends Sprite
 
-# Angle of the Earth to Sun to axis.
-var angle = 0
-
+# Radius of the planet.
 var radius = 1.0 # Ro
 
+# Initialization angle.
+var angle = 0
+
+# Radius of the planet.
 func radius():
 	return 1000 * global.au() * radius * global.EARTH_RADIUS / global.AU # px
 
+# Draw the planet at 500x its normal size.
 func _draw():
 	draw_circle(get_pos(), 500 * radius(), Color(0.0, 0.0, 1.0))
 
 func _ready():
-	print(radius() * global.AU / global.au())
 	set_process(true)
 
 func _process(delta):
-	# Find the Sun.
-	var sunPosition = global.get_viewport_center()
 	# Find your position.
 	var position = get_pos()
+	# Find the Sun.
+	var sunPosition = global.get_viewport_center()
 	# Find out how fast you are moving.
-	var orbitalSpeed = sqrt((global.G * global.stellarMass) / global.AU)
+	var orbitalSpeed = sqrt((global.G * global.stellarMass) / global.AU) # km/s
 	# Find the time warp.
 	var timeWarp = get_node("../../../Main/Panel/Time Warp").TIME_WARP
-	# Do the stuff.
-	angle += -(delta * orbitalSpeed * timeWarp) / global.AU
-
-
-	# if (angle > 2 * PI):
-	#	angle -= 2 * PI
-
+	# Process the angle.
+	angle += delta * orbitalSpeed * timeWarp / global.AU
+	if (angle > 2 * PI):
+		angle -= 2 * PI
+	# Update your position.
 	position.x = sunPosition.x + (cos(angle) * global.au())
-	position.y = sunPosition.y + (sin(angle) * global.au())
+	position.y = sunPosition.y - (sin(angle) * global.au())
+	# Set the new position.
 	set_pos(position)
