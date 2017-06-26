edbdeacc38df770de239c23a843b0cc104165ce9
wuddupdude <jeffrey.heroman@gmail.com>
2017-06-26 14:17:42 -0500

    Update space.gd

--- main/space/space.gd
+++ main/space/space.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-# Draw the sun in the middle of the viewport at 10x its normal size.
+# Draw the sun in the middle of the viewport at 20x its normal size.
 func _draw():
 	draw_circle(global.get_viewport_center(), 20 * global.star_radius(), Color(255.0, 255.0, 255.0))
